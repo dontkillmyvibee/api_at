24989f69b38254aca98e8a7e0cf0dccf03ff541c
dontkillmyvibee <lysorryoops@gmail.com>
2026-08-26 22:12:52 +0300

    feat: add negative get user test

--- assertions/users/assertions.py
+++ assertions/users/assertions.py
@@ -1,9 +1,12 @@
 import allure
+from requests import Response
+from pydantic import UUID4
 
 from clients.gateway.users.schemas import GetUserResponseSchema
 from fixtures.users import UserFixture
-from libs.assertions.base_assertions import assert_equal
+from libs.assertions.base_assertions import assert_equal, assert_in
 from libs.logger.logger import get_logger
+from libs.pydantic.error_schemas import HTTPInternalErrorSchema
 
 logger = get_logger("USER_ASSERTIONS")
 
@@ -18,3 +21,13 @@ def assert_get_user_response(user: UserFixture, response: GetUserResponseSchema)
     assert_equal(response.user.first_name, user.response_first_name, "first_name")
     assert_equal(response.user.middle_name, user.response_middle_name, "middle_name")
     assert_equal(response.user.phone_number, user.response_phone_number, "phone_number")
+
+
+@allure.step("Check user not found response")
+def assert_user_not_found(actual: Response, user_id: UUID4) -> None:
+    logger.info("Check user not found response")
+
+    expected = f"User with id {user_id} not found"
+    prep_actual = HTTPInternalErrorSchema.model_validate_json(actual.text)
+
+    assert_in(expected, prep_actual.detail, "detail")
--- tests/users/get/get_user/test_get_user.py
+++ tests/users/get/get_user/test_get_user.py
@@ -1,10 +1,11 @@
 from http import HTTPStatus
 
-from assertions.users.assertions import assert_get_user_response
+from assertions.users.assertions import assert_get_user_response, assert_user_not_found
 from clients.gateway.users.client import UsersHTTPClient
 from clients.gateway.users.schemas import GetUserResponseSchema
 from fixtures.users import UserFixture
 from libs.assertions.base_assertions import assert_status_code
+from libs.faker.faker import fake
 
 
 class TestGetUser:
@@ -14,3 +15,12 @@ class TestGetUser:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_get_user_response(user=function_user, response=response_data)
+
+
+class TestGetUserNegative:
+    def test_get_user_with_invalid_id(self, http_users_client: UsersHTTPClient) -> None:
+        user_id = fake.uuid()
+        response = http_users_client.get_user_api(user_id=user_id)
+
+        assert_status_code(response.status_code, HTTPStatus.NOT_FOUND)
+        assert_user_not_found(actual=response, user_id=user_id)
