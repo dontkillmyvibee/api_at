b23e199cda10e6032528cf003c447206cd64482e
dontkillmyvibee <dontkillmyvibee@users.noreply.github.com>
2026-09-02 13:13:38 +0000

    deploy: db3c510d18ce07e25a8e32375c002f31c287ce66

--- libs/http/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class HTTPRoutes(StrEnum):
-    USERS = "/api/v1/users"
-    CARDS = "/api/v1/cards"
-    ACCOUNTS = "/api/v1/accounts"
-    OPERATIONS = "/api/v1/operations"
-    DOCUMENTS = "/api/v1/documents"
